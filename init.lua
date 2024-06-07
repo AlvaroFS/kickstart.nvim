74585215931ce89c88d2bd3c3a94910d3cbfc19e
Álvaro <AlvaroFS@users.noreply.github.com>
2024-06-07 11:45:56 +0200

    Force trailing spaces removal on save

--- init.lua
+++ init.lua
@@ -223,6 +223,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Remove trailing spaces at save
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  pattern = { '*' },
+  callback = function(ev)
+    local save_cursor = vim.fn.getpos '.'
+    vim.cmd [[%s/\s\+$//e]]
+    vim.fn.setpos('.', save_cursor)
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
