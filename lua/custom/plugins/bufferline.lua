859bf255baae824217308c47e6adf5d0050fc893
Álvaro <AlvaroFS@users.noreply.github.com>
2025-01-15 11:35:38 +0100

    Add groups to buffer line and move tabs keymaps

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -9,6 +9,8 @@ return {
     { '<leader>vl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete Buffers to the Left' },
     { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
     { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+    { '<CS-h>', '<cmd>BufferLineMovePrev<cr>', desc = 'Move Buffer to the Left' },
+    { '<CS-l>', '<cmd>BufferLineMoveNext<cr>', desc = 'Move Buffer to the Right' },
     { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
     { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
   },
@@ -17,7 +19,7 @@ return {
       -- stylua: ignore
       close_command = function(n) require("mini.bufremove").delete(n, false) end,
       -- stylua: ignore
-      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+      middle_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
       diagnostics = 'nvim_lsp',
       always_show_bufferline = false,
       diagnostics_indicator = function(_, _, diag)
@@ -35,7 +37,38 @@ return {
           filetype = 'neo-tree',
           text = 'Neo-tree',
           highlight = 'Directory',
-          text_align = 'left',
+          text_align = 'center',
+        },
+      },
+      groups = {
+        options = {
+          toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+        },
+        items = {
+          {
+            name = 'Odin', -- Mandatory
+            auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
+            priority = 1, -- determines where it will appear relative to other groups (Optional)
+            matcher = function(buf) -- Mandatory
+              return buf.path:match '%.odin'
+            end,
+          },
+          {
+            name = 'Shaders', -- Mandatory
+            auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
+            priority = 2, -- determines where it will appear relative to other groups (Optional)
+            matcher = function(buf) -- Mandatory
+              return buf.path:match '%.glsl'
+            end,
+          },
+          {
+            name = 'Docs',
+            auto_close = true, -- whether or not close this group if it doesn't contain the current buffer
+            priority = 3, -- determines where it will appear relative to other groups (Optional)
+            matcher = function(buf)
+              return buf.path:match '%.md' or buf.path:match '%.txt'
+            end,
+          },
         },
       },
     },
